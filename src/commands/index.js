569b2412d77331c73eea54752f7e90ff34babffe
Saif Alesawy <justmesaif@yahoo.com>
2018-02-11 12:37:41 -0500

    ?itunes

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,7 @@
-exports.Cat = require('./Cat')
+exports.Cat = require('./Cat');
+exports.Define = require('./Define.js');
 exports.Flip = require('./Flip');
+exports.iTunes = require('./iTunes');
 exports.Roll = require('./Roll');
 exports.Rps = require('./Rps');
 exports.Quote = require('./Quote');
-exports.Define = require('./Define.js');

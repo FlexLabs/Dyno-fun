81f36c87f1cf4cc4d72b6737867527988357b0bb
Brian Tanner <brian@tanner.io>
2018-02-11 11:43:29 -0500

    fix aliases and indention in cat command

--- src/commands/Cat.js
+++ src/commands/Cat.js
@@ -5,8 +5,8 @@ const superagent = require('superagent');
 class Cat extends Command {
     constructor(...args) {
         super(...args);
-        this.name         = 'cat'
-        this.aliases      = ['kitty'];
+
+        this.aliases      = ['cat', 'kitty'];
         this.module        = 'Fun';
         this.description  = 'Find some cute cat pictures';
         this.usage        = 'cat';
@@ -15,25 +15,25 @@ class Cat extends Command {
         this.expectedArgs = 0;
     }
 
-async execute({ message }) {
-    try {
-            let res = await superagent.get('http://thecatapi.com/api/images/get?format=src&results_per_page=1');
-            return this.sendMessage(message.channel, {
-                embed: {
-                    title: "🐱 Meowww..",
-                    color: 0x3498db,
-                    image: {
-                        url: res.redirects[0]
-                    },
-                    footer: {
-                        text: res.redirects[0]
+    async execute({ message }) {
+        try {
+                let res = await superagent.get('http://thecatapi.com/api/images/get?format=src&results_per_page=1');
+                return this.sendMessage(message.channel, {
+                    embed: {
+                        title: "🐱 Meowww..",
+                        color: 0x3498db,
+                        image: {
+                            url: res.redirects[0]
+                        },
+                        footer: {
+                            text: res.redirects[0]
+                        }
                     }
-                }
-            });
-        } catch(err) {
-            return this.error(message.channel, 'Error: 😿 No cats found.');
+                });
+            } catch(err) {
+                return this.error(message.channel, 'Error: 😿 No cats found.');
+            }
         }
-    }
 }
 
 module.exports = Cat;

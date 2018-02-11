45f2c49a8edaa516cc745b6b951bffa492335ce0
CoalSephos <31113295+CoalSephos@users.noreply.github.com>
2018-02-11 17:04:26 +0000

    add dog

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,4 +1,5 @@
-exports.Cat = require('./Cat')
+exports.Cat = require('./Cat');
+exports.Dog = require('.Dog');
 exports.Flip = require('./Flip');
 exports.Roll = require('./Roll');
 exports.Rps = require('./Rps');

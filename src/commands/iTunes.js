07c06365f121677a86512e9d2b9296d1ed9d0f3c
Saif Alesawy <justmesaif@yahoo.com>
2018-02-11 13:12:25 -0500

    thanks gin

--- src/commands/iTunes.js
+++ src/commands/iTunes.js
@@ -15,49 +15,51 @@ class iTunes extends Command {
 	}
 
 	async execute({ message, args }) {
-        args = args.join(' ');
-	    try {
-            let res = await superagent.get('https://itunes.apple.com/search').query({ term: args, media: 'music' });
-            res = JSON.parse(res.text);
+		args = args.join(' ');
+		try {
+			let res = await superagent.get('https://itunes.apple.com/search').query({ term: args, media: 'music' });
+			res = JSON.parse(res.text);
 
-            let artist = res.results[0].artistName;
-            let artistLink = res.results[0].artistViewUrl;
-            let song = res.results[0].trackName;
-            let songLink = res.results[0].trackViewUrl;
-            let icon = res.results[0].artworkUrl100;
-            let album = res.results[0].collectionName;
-            let albumLink = res.results[0].collectionViewUrl;
-            let genre = res.results[0].primaryGenreName;
+			let artist = res.results[0].artistName;
+			let artistLink = res.results[0].artistViewUrl;
+			let song = res.results[0].trackName;
+			let songLink = res.results[0].trackViewUrl;
+			let icon = res.results[0].artworkUrl100;
+			let album = res.results[0].collectionName;
+			let albumLink = res.results[0].collectionViewUrl;
+			let genre = res.results[0].primaryGenreName;
 
-		    return this.sendMessage(message.channel, {embed: {
-                author: {
-                    name: song,
-                    icon_url: icon
-                },
-                title: `Song info:`,
-                description: `[${song}](${songLink})`,
-                color: 0x3498db,
-                fields: [
-                    {
-                        name: 'Artist:',
-                        value: `[${artist}](${artistLink})`
-                    },
-                    {
-                        name: 'Album:',
-                        value: `[${album}](${albumLink})`
-                    }
-                ],
-                thumbnail: {
-                    url: icon
-                },
-                footer: {
-                    text: `Genre: ${genre}`
-                },
-                timestamp: new Date()
-            }});
-	    } catch(err) {
-	    	return this.error(message.channel, 'An error occured: Unable to fetch song.');
-	    }
+			return this.sendMessage(message.channel, {
+				embed: {
+					author: {
+						name: song,
+						icon_url: icon
+					},
+					title: `Song info:`,
+					description: `[${song}](${songLink})`,
+					color: 0x3498db,
+					fields: [
+						{
+							name: 'Artist:',
+							value: `[${artist}](${artistLink})`
+						},
+						{
+							name: 'Album:',
+							value: `[${album}](${albumLink})`
+						}
+					],
+					thumbnail: {
+						url: icon
+					},
+					footer: {
+						text: `Genre: ${genre}`
+					},
+					timestamp: new Date()
+				}
+			});
+		} catch (err) {
+			return this.error(message.channel, 'An error occured: Unable to fetch song.');
+		}
 	}
 }
 

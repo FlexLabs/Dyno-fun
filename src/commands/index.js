5f271ca4496074e12f8ec68302cb720bcfd51775
CoalSephos <31113295+CoalSephos@users.noreply.github.com>
2018-02-11 17:04:44 +0000

    typo

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,5 @@
 exports.Cat = require('./Cat');
-exports.Dog = require('.Dog');
+exports.Dog = require('./Dog');
 exports.Flip = require('./Flip');
 exports.Roll = require('./Roll');
 exports.Rps = require('./Rps');

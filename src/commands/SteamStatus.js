bbddfb12bf63096d9ebd225a3a34502ee4aa5927
Sam Fry <benolot@gmail.com>
2018-02-12 16:19:40 +0000

    Updated embed design

--- src/commands/SteamStatus.js
+++ src/commands/SteamStatus.js
@@ -17,47 +17,48 @@ class SteamStatus extends Command {
 
     async execute({ message }) {
         try {
-                let res = await superagent.get('https://steamgaug.es/api/v2');
-                let icon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/2000px-Steam_icon_logo.svg.png';
-                let steamcommunity = res.body.SteamCommunity;
-                let steamstore = res.body.SteamStore;
-                let steamclient = res.body.ISteamClient;
+            let res = await superagent.get('https://steamgaug.es/api/v2');
+            let icon = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/2000px-Steam_icon_logo.svg.png';
+            let steamcommunity = res.body.SteamCommunity;
+            let steamstore = res.body.SteamStore;
+            let steamclient = res.body.ISteamClient;
 
-                console.log(res.body);
+            let steamcommunitystatus = steamcommunity.online ? `Online ${this.config.emojis.success}` : `Offline ${this.config.emojis.error}`;
+            let steamclientstatus = steamclient.online ? `Online ${this.config.emojis.success}` : `Offline ${this.config.emojis.error}`;
+            let steamstorestatus = steamstore.online ? `Online ${this.config.emojis.success}` : `Offline ${this.config.emojis.error}`;
 
-                steamcommunity.online ? 'Online' : 'Offline';
-                steamclientstatus.online ? 'Online' : 'Offline';
-                steamstorestatus.online ? 'Online' : 'Offline';
-
-                return this.sendMessage(message.channel, {
-                    embed: {
-                        color: 0x666666,
-                        author: {
-                            name: 'Steam Status via steamgaug.es',
-                            icon_url: icon
+            return this.sendMessage(message.channel, {
+                embed: {
+                    color: 0x666666,
+                    author: {
+                        name: 'Steam Status',
+                        icon_url: icon
+                    },
+                    title: 'Steam Community',
+                    description: steamcommunitystatus,
+                    fields: [
+                        {
+                            name: 'Steam Client',
+                            value: steamclientstatus
                         },
-                        title: 'Steam Community',
-                        description: steamcommunitystatus,
-                        fields: [
-                            {
-                                name: 'Steam Client:',
-                                value: steamclientstatus
-                            },
-                            {
-                                name: 'Steam Store:',
-                                value: steamstorestatus
-                            }
-                        ],
-                        thumbnail: {
-                            url: icon
-                        },
-                        timestamp: new Date()
+                        {
+                            name: 'Steam Store',
+                            value: steamstorestatus
+                        }
+                    ],
+                    thumbnail: {
+                        url: icon
+                    },
+                    timestamp: new Date(),
+                    footer: {
+                        text: "via steamgaug.es"
                     }
-                });
-            } catch(err) {
-                return this.error(message.channel, 'Error! Unable to fetch steam status.');
-            }
+                }
+            });
+        } catch(err) {
+            return this.error(message.channel, 'Error! Unable to fetch steam status.');
         }
+    }
 }
 
 module.exports = SteamStatus;

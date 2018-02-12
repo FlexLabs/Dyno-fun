5ac29f3535be0731e31829d24821b6e70a2baf01
Brian Tanner <brian@tanner.io>
2018-02-12 12:06:02 -0500

    add pug command

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,6 +3,7 @@ exports.Dog = require('./Dog');
 exports.Define = require('./Define.js');
 exports.Flip = require('./Flip');
 exports.iTunes = require('./iTunes');
+exports.Pug = require('./Pug');
 exports.Roll = require('./Roll');
 exports.Rps = require('./Rps');
 exports.Quote = require('./Quote');

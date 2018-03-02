d8c4f829ca667b95c3282388237d7f24d3565951
MikaKpk <32469440+MikaKpk@users.noreply.github.com>
2018-03-02 22:47:08 +0100

    styling update + removal of points

--- src/commands/Slots.js
+++ src/commands/Slots.js
@@ -4,7 +4,7 @@ class Slots extends Command {
     constructor(...args) {
         super(...args);
 
-        this.aliases      = ['slots'];
+       	this.aliases      = ['slots'];
         this.module       = 'Fun';
         this.description  = 'Spin the slots and win the jackpot! ...or loose everything.';
         this.usage        = 'slots';
@@ -12,42 +12,43 @@ class Slots extends Command {
         this.cooldown     = 5000;
         this.expectedArgs = 0;
     }
+
 	checkJackpot(gameid, bonus) {
-		if(gameid === "555" && bonus === 15) return true //basically just checking if the highest possible score was reached
-		else return false 
+		if(gameid === "555" && bonus === 15) { return true } //basically just checking if the highest possible score was reached
+		else { return false }; 
 	}
 	
 	checkTriple(frst, scnd, thrd, gameid) {
-		const special = ["154", "215", "512", "451"] //these gameIDs are valid triples even though they do not follow the standard scheme
+		const special = ["154", "215", "512", "451"]; //these gameIDs are valid triples even though they do not follow the standard scheme
 		
-		if(frst === scnd && scnd === thrd) return true //If all emojis on the middle line are the same
-		else if(frst - 1 === scnd && scnd === thrd + 1) return true  //If all emojis on the diagonal starting at the bottom left are the same
-		else if(frst + 1 === scnd && scnd === thrd - 1) return true //if all emojis on the diagonal starting at the top left are the same
-		else if(special.indexOf(gameid) > -1) return true //If it doesnt follow the scheme but is still a valid triple
-		else return false
+		if(frst === scnd && scnd === thrd) { return true } //If all emojis on the middle line are the same
+		else if(frst - 1 === scnd && scnd === thrd + 1) { return true }  //If all emojis on the diagonal starting at the bottom left are the same
+		else if(frst + 1 === scnd && scnd === thrd - 1) { return true } //if all emojis on the diagonal starting at the top left are the same
+		else if(special.indexOf(gameid) > -1) { return true } //If it doesnt follow the scheme but is still a valid triple
+		else { return false };
 	}
 	
 	checkDouble(frst, scnd, thrd) {
-		if(frst === scnd || scnd === thrd) return true //If either the first or the last two on the middle line are the same
-		else return false
+		if(frst === scnd || scnd === thrd) { return true } //If either the first or the last two on the middle line are the same
+		else { return false };
 	}
 
 	execute({ msg }) {
 
-		const emojis = [":bullettrain_front:",":white_sun_small_cloud:",":lion_face:",":earth_africa:",":crown:",":bullettrain_front:",":white_sun_small_cloud:"] //Insert emoji strings of whatever you like
+		const emojis = [":bullettrain_front:",":white_sun_small_cloud:",":lion_face:",":earth_africa:",":crown:",":bullettrain_front:",":white_sun_small_cloud:"]; //Insert emoji strings of whatever you like
 
-		let frst = Math.floor(Math.random() * 5) + 1
-		let scnd = Math.floor(Math.random() * 5) + 1
-		let thrd = Math.floor(Math.random() * 5) + 1
-		let bonus = Math.floor(Math.random() * 10) + 6
-		let gameid = frst.toString() + scnd.toString() + thrd.toString()
+		let frst = Math.floor(Math.random() * 5) + 1;
+		let scnd = Math.floor(Math.random() * 5) + 1;
+		let thrd = Math.floor(Math.random() * 5) + 1;
+		let bonus = Math.floor(Math.random() * 10) + 6;
+		let gameid = frst.toString() + scnd.toString() + thrd.toString();
 
-		let result = `\n<:dynov4:341008963254484992> ***DYNO SLOTS*** <:dynov4:341008963254484992>\n\n`
-		result += `--------------------------\n`
-		result += `\\\u2198     ${emojis[frst - 1]}  ${emojis[scnd - 1]}  ${emojis[thrd - 1]}     \\\u2199\n\n`
-		result += `\\\u25B6    ${emojis[frst]}  ${emojis[scnd]}  ${emojis[thrd]}    \\\u25C0\n\n`
-		result += `\\\u2197     ${emojis[frst + 1]}  ${emojis[scnd + 1]}  ${emojis[thrd + 1]}     \\\u2196`
-		result += `\n--------------------------\n\n\n`
+		let result = `\n<:dynov4:341008963254484992> ***DYNO SLOTS*** <:dynov4:341008963254484992>\n\n`;
+		result += `--------------------------\n`;
+		result += `\\\u2198     ${emojis[frst - 1]}  ${emojis[scnd - 1]}  ${emojis[thrd - 1]}     \\\u2199\n\n`;
+		result += `\\\u25B6    ${emojis[frst]}  ${emojis[scnd]}  ${emojis[thrd]}    \\\u25C0\n\n`;
+		result += `\\\u2197     ${emojis[frst + 1]}  ${emojis[scnd + 1]}  ${emojis[thrd + 1]}     \\\u2196`;
+		result += `\n--------------------------\n\n\n`;
 
 		msg.channel.createMessage({
 			embed:{
@@ -55,60 +56,57 @@ class Slots extends Command {
 				description:`Spinning the wheels of fortune for [${msg.member.username}](https://dynobot.net)...`,
 				color: 1259143,
 				thumbnail: {
-	      				url: "https://s18.postimg.org/hmpvac5qh/dynoslots_spin.gif"
-	    			}
+	      			url: "https://s18.postimg.org/hmpvac5qh/dynoslots_spin.gif"
+	    		}
 			}
 		}).then(newmsg => {
 			if(checkJackpot(gameid, bonus)) {
-				let points = "`" + bonus.toString() + gameid + "`";
 				setTimeout(function() { msg.channel.editMessage(newmsg.id, {
 					embed:{
 						title:"JACKPOT!",
 						url:"https://dynobot.net/",
 						color: 13496340,
-						description:`${result}**Woah, thats a Jackpot!** You reached the highest possible score of ${points} points, [${msg.member.username}](https://dynobot.net)!`,
+						description:`${result}**Woah, thats a Jackpot!** You were lucky and reached the best result in this game possible!\nNow thats a thing to screenshot and share with your friends, [${msg.member.username}](https://dynobot.net)!`,
 						footer:{
 							text:`Game played by ${msg.member.username}#${msg.member.discriminator}`,
 							icon_url:msg.member.avatarURL
 						},
 						thumbnail: {
-	      						url: "https://s17.postimg.org/w4v2gei1r/tumblr_mh79cf_ON001r88gpzo1_400.gif"
-	    					}
+	      					url: "https://s17.postimg.org/w4v2gei1r/tumblr_mh79cf_ON001r88gpzo1_400.gif"
+	    				}
 					}
 				}) }, 2000)		
-			}//this thing was made by Mika K.#2002 btw... If my discrim should change in the future or I should go missing, my user ID on Discord is 264762392683085834
+			} //this thing was made by Mika K.#2002 btw... If my discrim should change in the future or I should go missing, my user ID on Discord is 264762392683085834
 			else if(checkTriple(frst, scnd, thrd, gameid)) {
-				let points = "`" + bonus.toString() + gameid + "`";
 				setTimeout(function() { msg.channel.editMessage(newmsg.id, {
 					embed:{
 						title:"Congratulations!",
 						url:"https://dynobot.net/",
 						color: 0x337fd5,
-						description:`${result}**You won,** and earned ${points} points, [${msg.member.username}](https://dynobot.net)!`,
+						description:`${result}You got **three in a row**, [${msg.member.username}](https://dynobot.net). Amazing!`,
 						footer:{
 							text:`Game played by ${msg.member.username}#${msg.member.discriminator}`,
 							icon_url:msg.member.avatarURL
 						},
 						thumbnail: {
 							url:"https://s18.postimg.org/u6kzxx9bd/dynoslots_win2.gif"
-	    					}
+	    				}
 					}
 				}) }, 2000)
 			}
 			else if(checkDouble(frst, scnd, thrd)) {
-				let points = "`" + gameid + "`";
 				setTimeout(function() { msg.channel.editMessage(newmsg.id, {
 					embed:{
 						title:"Thats a pair!",
 						url:"https://dynobot.net/",
 						color: 8773445,
-						description:`${result}**Thats a pair!** You earned ${points} points, [${msg.member.username}](https://dynobot.net)!`,
+						description:`${result}**Thats a pair!**, [${msg.member.username}](https://dynobot.net)! Not bad.`,
 						footer:{
 							text:`Game played by ${msg.member.username}#${msg.member.discriminator}`,
 							icon_url:msg.member.avatarURL
 						},
 						thumbnail: {
-	      						url: "https://s17.postimg.org/j0pi3qd5b/giphy.gif"
+	      					url: "https://s17.postimg.org/j0pi3qd5b/giphy.gif"
 	   		 			}
 					}
 				}) }, 2000)
@@ -125,8 +123,8 @@ class Slots extends Command {
 							icon_url:msg.member.avatarURL
 						},
 						thumbnail: {
-	    	  					url:"https://s17.postimg.org/dcj7cxgjj/giphy.gif"
-	    					}
+	    	  				url:"https://s17.postimg.org/dcj7cxgjj/giphy.gif"
+	    				}
 					}
 				}) }, 2000)
 			}
@@ -134,4 +132,4 @@ class Slots extends Command {
 	}
 }
 
-module.exports = Slots
+module.exports = Slots;

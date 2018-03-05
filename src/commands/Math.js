0994b2b5dfb7d4673320b84c274a25aec041c16f
Ape <31214445+bjornmorten@users.noreply.github.com>
2018-03-05 17:04:20 +0100

    Updated math code

--- src/commands/Math.js
+++ src/commands/Math.js
@@ -2,10 +2,6 @@ const { Command } = require('@dyno.gg/dyno-core');
 const superagent = require('superagent');
 const math = require('mathjs');
 //
-
-function hasTypes(i) {
-	return /[+-/*^%]/.test(i);
-};	
 async function request(i) {
    	return await superagent.get(`http://numbersapi.com/${i}?json`);
 };  
@@ -15,18 +11,18 @@ class Math extends Command {
 	constructor(...args) {
 		super(...args);
 		
-		this.aliases      = ['math'];
+		this.aliases      = ['math', 'calc'];
 		this.module       = 'Fun';
 		this.description  = 'Math command';
-		this.usage        = 'math [math expression]';
-		this.example      = 'math 152 + 37';
+		this.usage        = ['math [math expression]', 'math fact'];
+		this.example      = ['math 152 + 37', 'math pi', 'math fact', 'math 47', 'math 34 fahrenheit to celsius', 'math 12.7 cm to inch'];
 		this.cooldown     = 3000;
 		this.expectedArgs = 1;
 	}
 
 	async execute({ message, args }) {
 		const input = args.join(' ');
-        const errmsg = {
+    	const errmsg = {
             exp: 'Couldn\'t evaluate the given expression',
             err: 'An Error Occured',
             fact: 'Couldn\'t get any facts for that number'
@@ -38,45 +34,7 @@ class Math extends Command {
             result.embed.fields = [];
             result.embed.color = 0x41dae2;
 
-        if(args.length > 1) { // If args is more than 1 word, example: 1 + 1
-            try {
-                output = math.eval(input).toString();
-
-                result.embed.fields.push({
-                    name: 'Input',
-                    value: input,
-                    inline: false
-                });
-
-                result.embed.fields.push({
-                    name: 'Output',
-                    value: output,
-                    inline: false
-                });
-
-            } catch (e) {
-                result = errmsg.exp;
-            };
-        } else if(args.length == 1 && hasTypes(args[0])) { // If args is a math expression in 1 word, example: 1+1
-            try {
-                output = math.eval(input).toString();
-
-                result.embed.fields.push({
-                    name: 'Input',
-                    value: input,
-                    inline: false
-                });
-
-                result.embed.fields.push({
-                    name: 'Output',
-                    value: output,
-                    inline: false
-                });
-
-            } catch (e) {
-                result = errmsg.exp;
-            };
-        } else if(args[0] == 'fact') { // Get a fact about a random number
+        if(args[0].toLowerCase() == 'fact') {
             try {
 
                 res = await request('random/math');
@@ -91,10 +49,27 @@ class Math extends Command {
             } catch (e) {
                 result = errmsg.err;
             };
-        } else if(args.length == 1 && isNaN(args[0])) { // If args is 1 word and NaN, example: pi 
+
+            return this.sendMessage(message.channel, result);
+        }
+
+        if(args.length === 1 && !isNaN(args[0])) {
+                const ainput = (input.startsWith('.')) ? '0' + input : input;
+                res = await request(`${ainput.split('.')[0]}/math`);
+
+                if(res.body.found) {
+                    result.embed.fields.push({
+                        name: `Fact about ${res.body.number}`,
+                        value: res.body.text,
+                        inline: false
+                    });
+                } else {
+                    result = errmsg.fact;
+                };
+        } else {
             try {
                 output = math.eval(input).toString();
-                
+
                 result.embed.fields.push({
                     name: 'Input',
                     value: input,
@@ -107,34 +82,11 @@ class Math extends Command {
                     inline: false
                 });
 
-                res = await request(`${output.split('.')[0]}/math`);
-
-                if(res.body.found) {
-                    result.embed.fields.push({
-                        name: `Fact about ${res.body.number}`,
-                        value: res.body.text,
-                        inline: false
-                    });
-                }
             } catch (e) {
                 result = errmsg.exp;
             };
-        } else { // If args is just a number, not an experssion, example: 123
-            output = math.eval(input).toString();
-
-            res = await request(`${output.split('.')[0]}/math`);
-
-            if(res.body.found) {
-                result.embed.fields.push({
-                    name: `Fact about ${res.body.number}`,
-                    value: res.body.text,
-                    inline: false
-                });
-            } else {
-            result = errmsg.fact;
-            }
-        }
-
+        };
+        
         return this.sendMessage(message.channel, result);
 	}
 }

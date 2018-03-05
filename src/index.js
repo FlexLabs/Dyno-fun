a2a5202e6f04c3c8d360612060ab56cd18e242e0
Germano Eichenberg <germanoeich@gmail.com>
2018-03-04 23:42:15 -0300

    fixed the lint

--- src/index.js
+++ src/index.js
@@ -10,15 +10,14 @@ class Fun extends Module {
 	constructor(...args) {
 		super(...args);
 		this.module = 'Fun';
-			
-		this.friendlyName = 'Fun'; 
+		this.friendlyName = 'Fun';
 		this.description = 'Adds fun commands to your server!';
 		this.list = true;
 		this.enabled = true;
 		this.hasPartial = false;
 		this.commands = commands;
 	}
-
+	// ok fixed the pr
 	// eslint-disable-next-line no-empty-function
 	start() { }
 }

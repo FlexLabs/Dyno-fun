2c0a1d40534650d6becf5fceeeaf0b9fc616cb7f
Brian Tanner <brian@tanner.io>
2018-03-06 14:03:01 -0500

    refactor github command fixing several bugs

--- src/commands/Github.js
+++ src/commands/Github.js
@@ -16,68 +16,80 @@ class Github extends Command {
 
     async execute({ message, args }) {
         args = args.join(' ');
+
         let url;
+        let res;
+        let embed;
+
         if (args.includes('/')) {
-            args = args.split('/');
-            url = `https://api.github.com/repos/${args[0]}/${args[1]}`;
+            let parts = args.split('/');
+            url = `https://api.github.com/repos/${parts[0]}/${parts[1]}`;
         } else {
             url = `https://api.github.com/search/repositories?q=${args}`;
         }
 
         try {
-            let res = await superagent.get(url);
-            if (url === `https://api.github.com/search/repositories?q=${args}`) res.body = res.body.items[0];
-            return this.sendMessage(message.channel, {
-                embed: {
-                    author: {
-                        name: res.body.owner.login,
-                        url: res.body.owner.html_url,
-                        icon_url: res.body.owner.avatar_url,
-                    },
-                    thumbnail: {
-                        url: res.body.owner.avatar_url,
-                    },
-                    fields: [
-                        {
-                            name: 'Repository:',
-                            value: `[${res.body.name}](${res.body.html_url})`,
-                            inline: true,
-                        },
-                        {
-                            name: 'Most Used Language:',
-                            value: res.body.language,
-                            inline: true,
-                        },
-                        {
-                            name: 'Forks:',
-                            value: res.body.forks_count,
-                            inline: true,
-                        },
-                        {
-                            name: 'Watchers:',
-                            value: res.body.watchers_count,
-                            inline: true,
-                        },
-                        {
-                            name: `Open Issues:`,
-                            value: res.body.open_issues_count,
-                            inline: true,
-                        },
-                        {
-                            name: 'License:',
-                            value: res.body.license.name,
-                            inline: true,
-                        },
-                    ],
-                    timestamp: res.body.created_at,
-                    footer: {
-                        text: 'Repo created at ',
-                    },
-                },
-            });
+            const result = await superagent.get(url);
+
+            if (result.body && result.body.items && result.body.items.length) {
+                res = result.body.items[0];
+            } else if (result.body) {
+                res = result.body;
+            } else {
+                return this.error(message.channel, `I didn't find a repository matching that name.`);
+            }
         } catch (err) {
             return this.error(message.channel, 'An error has occured: Unable to fetch repository');
         }
+
+        try {
+            embed = {
+                author: {
+                    name: res.owner.login,
+                    url: res.owner.html_url,
+                    icon_url: res.owner.avatar_url,
+                },
+                thumbnail: {
+                    url: res.owner.avatar_url,
+                },
+                fields: [
+                    {
+                        name: 'Repository',
+                        value: `[${res.name}](${res.html_url})`,
+                        inline: true,
+                    },
+                ],
+                timestamp: res.created_at,
+                footer: {
+                    text: 'Repo created at ',
+                },
+            };
+
+            if (res.language) {
+                embed.fields.push({ name: 'Most Used Language', value: res.language, inline: true });
+            }
+
+            if (res.forks_count) {
+                embed.fields.push({ name: 'Forks', value: res.forks_count.toString(), inline: true });
+            }
+
+            if (res.watchers_count) {
+                embed.fields.push({ name: 'Watchers', value: res.watchers_count.toString(), inline: true });
+            }
+
+            if (res.open_issues_count) {
+                embed.fields.push({ name: 'Open Issues', value: res.open_issues_count.toString(), inline: true });
+            }
+
+            if (res.license && res.license.name) {
+                embed.fields.push({ name: 'License', value: res.license.name, inline: true });
+            }
+        } catch (err) {
+            this.logger.error(err);
+            return this.error(message.channel, `Something went wrong. Please try again.`);
+        }
+
+        return this.sendMessage(message.channel, { embed });
     }
 }
 

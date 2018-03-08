152ce5a023424798fe3e936eb587c3c5d7974d68
Crispyｨ™ <life.so.crispy@gmail.com>
2018-03-08 17:58:38 -0500

    final fix i think

--- src/commands/Trivia.js
+++ src/commands/Trivia.js
@@ -5,25 +5,24 @@ class Trivia extends Command {
 	constructor(...args) {
 		super(...args);
 
-		this.aliases        = ['trivia']; 
+		this.aliases        = ['trivia'];
 		this.module         = 'Fun';
 		this.description    = 'Trivia time, get some friends and get a trivia game started! ';
 		this.usage          = 'trivia';
 		this.example        = 'trivia';
 		this.cooldown       = 5000;
 		this.expectedArgs   = 0;
-	}	
+	}
 
 		async execute({ message }) {
 			try {
-				let res = await superagent.get('http://jservice.io/api/random'); // http://jservice.io/api/random is the api site, there is so much to add.  
-				
-               	 	return this.sendMessage(message.channel, res.body[0].question);
-	
+				let res = await superagent.get('http://jservice.io/api/random'); // http://jservice.io/api/random is the api site, there is so much to add.
+
+				return this.sendMessage(message.channel, res.body[0].question);
 			} catch (err) {
 				return this.error(message.channel, 'No Trivia questions found... Something went wrong');
 			}
 		}
 	}
-	
-module.exports = Trivia; 
+
+module.exports = Trivia;

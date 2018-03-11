39678728d98c878a6ddc6109e779e7368ff7711a
Crispyｨ™ <life.so.crispy@gmail.com>
2018-03-11 12:14:45 -0400

    Added Promise.all statement to Bunnies.vb

--- src/commands/Bunnies.js
+++ src/commands/Bunnies.js
@@ -6,7 +6,7 @@ class Bunnies extends Command {
     constructor(...args) {
         super(...args);
 
-        this.aliases      = ['bunny', 'bunnies']; // fun fact, i've been bitten by 2 Bunnies in my life time. ~Crispy
+        this.aliases      = ['bunny', 'bunnies']; // I have been bitten by 2 Bunnies in my lifetime.
         this.module       = 'Fun';
         this.description  = 'Random Adorable Bunnies';
         this.usage        = 'bunny';
@@ -23,10 +23,13 @@ class Bunnies extends Command {
 				{ search: 'Looking for a bunnies...', found: 'Found one!' },
 			];
 
-			const response = responses[utils.getRandomInt(0, responses.length - 1)];
-			const msg = await this.sendMessage(message.channel, response.search);
+            const [res, msg] = await Promise.all([
+                superagent.get('https://api.bunnies.io/v2/loop/random/?media=poster'),
+                this.sendMessage(message.channel, response.search),
 
-			let res = await superagent.get('https://api.bunnies.io/v2/loop/random/?media=poster');
+            ]);
+
+            const response = responses[utils.getRandomInt(0, responses.length - 1)];
 
 			return msg.edit({
 				content: response.found,

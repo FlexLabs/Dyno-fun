a2b0e5e4d7698ec79f8492fd0463290a6a27770f
JohnCoder <johnsmithcoder7@gmail.com>
2018-04-05 22:34:31 -0400

    add prefetcher class

---
+++ src/Prefetcher.js
@@ -0,0 +1,27 @@
+const superagent = require('superagent');
+
+class Prefetcher {
+    constructor(url, options) {
+        this.url = url;
+        this.options = options;
+        this.cache = [];
+    }
+
+    get() {
+        let item = this.cache[0];
+        this.cache.splice(0, 1);
+        this.prefetch();
+        return item;
+    }
+
+    async prefetch() {
+        let res = await superagent
+            .get(this.url)
+            .set({ Authorization: this.options.auth ? this.options.auth : '' })
+            .set({ Accept: this.options.accept ? this.options.accept : 'application/json' });
+
+        this.cache.push(res);
+    }
+}
+
+module.exports = Prefetcher;

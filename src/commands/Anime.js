45f8efbcba56e62f7aad5ce0b655db0ab6b1481e
osuletz <38139840+osuletz@users.noreply.github.com>
2018-04-07 23:29:32 +0300

    Add files via upload
    retrying, changed let to const, gif link and deleted inline from last field

--- src/commands/Anime.js
+++ src/commands/Anime.js
@@ -6,13 +6,13 @@ class Anime extends Command {
 	constructor(...args) {
 		super(...args);
 
-		this.name         = 'anime';
-		this.aliases      = ['animu'];
-		this.module       = 'Fun';
-		this.description  = 'Get info about an anime';
-		this.usage        = 'anime <anime>';
-		this.example      = 'anime acchi kocchi';
-		this.cooldown     = 5000;
+		this.name = 'anime';
+		this.aliases = ['animu'];
+		this.module = 'Fun';
+		this.description = 'Get info about an anime';
+		this.usage = 'anime <anime>';
+		this.example = 'anime acchi kocchi';
+		this.cooldown = 5000;
 		this.expectedArgs = 1;
 	}
 
@@ -24,58 +24,59 @@ class Anime extends Command {
 					url: 'https://cdn.dyno.gg/fun/discord_loading.gif',
 				},
 				title: 'Looking for an anime...',
-			}
+			},
 		});
-		try {
-				let res = await superagent.get(`https://api.jikan.me/search/anime/${args.slice(1).join(' ')}/1`);
-				let anime = res.body.result[0];
+		let res;
+		let anime;
 
-				return msg.edit({
-					embed: {
-						author: {
-							name: `${anime.title}`,
-							url: `${anime.url}`,
-							icon_url: 'https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png',
-						},
-						color: 0x337fd5,
-						thumbnail: {
-							url: anime.image_url,
-						},
-						timestamp: new Date(),
-						fields: [ 
-							{
-								name: 'Episodes',
-								value: `${anime.episodes}`,
-								inline: true,
-							},
-							{
-								name: 'Type',
-								value: `${anime.type}`,
-								inline: true,
-							},
-							{
-								name: 'Score',
-								value: `${anime.score}`,
-								inline: true,
-							},
-							{
-								name: 'Members',
-								value: `${anime.members}`,
-								inline: 	true,
-							},
-							{
-								name: 'Description',
-								value: `${anime.description}`,
-							}
-						],
-					},
-				});
+		try {
+			res = await superagent.get(`https://api.jikan.me/search/anime/${args.slice(1).join(' ')}/1`);
+			anime = res.body.result[0];
 		} catch (err) {
 			return msg.edit({
-				content: `${this.utils.emoji.error} I couldn't find any.`, 
-				embed: {}
-			})
+				content: `${this.utils.emoji.error} I couldn't find any.`,
+				embed: {},
+			});
 		}
+		return msg.edit({
+			embed: {
+				author: {
+					name: `${anime.title}`,
+					url: `${anime.url}`,
+					icon_url: 'https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png',
+				},
+				color: 0x337fd5,
+				thumbnail: {
+					url: anime.image_url,
+				},
+				timestamp: new Date(),
+				fields: [{
+						name: 'Episodes',
+						value: `${anime.episodes}`,
+						inline: true,
+					},
+					{
+						name: 'Type',
+						value: `${anime.type}`,
+						inline: true,
+					},
+					{
+						name: 'Score',
+						value: `${anime.score}`,
+						inline: true,
+					},
+					{
+						name: 'Members',
+						value: `${anime.members}`,
+						inline: true,
+					},
+					{
+						name: 'Description',
+						value: `${anime.description}`,
+					},
+				],
+			},
+		});
 	}
 }
 
